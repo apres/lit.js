6c71e7b8d7f068979a23e492d82750ad9b76184b
Casey Duncan <casey.duncan@gmail.com>
2012-06-30 14:16:42 -0600

    Add server.js script to test locally

---
+++ server.js
@@ -0,0 +1,12 @@
+var http = require('http'),
+    static = require('node-static');
+
+var file = new (static.Server)('./');
+
+http.createServer(function(req, res) {
+  req.addListener('end', function() {
+    file.serve(req, res);
+  });
+}).listen(6000);
+
+console.log("Lit-examples now being server at http://localhost:5000");
