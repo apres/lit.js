59095dd09600580f445afb9a575a0ef74a584f87
Casey Duncan <casey.duncan@gmail.com>
2012-07-01 20:25:37 -0600

    fix broken test

--- test/lit-test.js
+++ test/lit-test.js
@@ -110,25 +110,25 @@ test('#section structure', function() {
 test('#section content', function() {
   var lit = require('../lib/lit');
   var sections = [
-    {comments: 'Comment 1', code: 'Code 1'},
-    {comments: 'Comment 2', code: 'Code 2'},
-    {comments: 'Comment 3', code: 'Code 3'},
+    {comments: 'Comment 1', code: 'Code One'},
+    {comments: 'Comment 2', code: 'Code Two'},
+    {comments: 'Comment 3', code: 'Code Three'},
   ];
   var html = lit.makeHtml(sections);
   var split = html.split('<section>');
   assert.equal(split.length, 4);
-  assert.notEqual(split[1].indexOf('Code 1'), -1);
-  assert.notEqual(split[1].indexOf('Comment 1'), -1);
-  assert.equal(split[1].indexOf('Code 2'), -1);
-  assert.equal(split[1].indexOf('Code 3'), -1);
-  assert.notEqual(split[2].indexOf('Code 2'), -1);
-  assert.notEqual(split[2].indexOf('Comment 2'), -1);
-  assert.equal(split[2].indexOf('Code 1'), -1);
-  assert.equal(split[2].indexOf('Code 3'), -1);
-  assert.notEqual(split[3].indexOf('Code 3'), -1);
-  assert.notEqual(split[3].indexOf('Comment 3'), -1);
-  assert.equal(split[3].indexOf('Code 1'), -1);
-  assert.equal(split[3].indexOf('Code 2'), -1);
+  assert.include(split[1], 'Code One');
+  assert.include(split[1], 'Comment 1');
+  assert.equal(split[1].indexOf('Code Two'), -1);
+  assert.equal(split[1].indexOf('Code Three'), -1);
+  assert.include(split[2], 'Code Two');
+  assert.include(split[2], 'Comment 2');
+  assert.equal(split[2].indexOf('Code One'), -1);
+  assert.equal(split[2].indexOf('Code Three'), -1);
+  assert.include(split[3], 'Code Three');
+  assert.include(split[3], 'Comment 3');
+  assert.equal(split[3].indexOf('Code One'), -1);
+  assert.equal(split[3].indexOf('Code Two'), -1);
 });
 
 test('#preprocess collapses empty lines', function() {
